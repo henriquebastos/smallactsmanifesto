ee4f4619923a4c2c724805292f91942183fb6d41
Henrique Bastos <henrique@bastos.net>
2010-02-14 18:58:21 -0200

    Added list view to show all the active signatories.

--- app/signatures/urls.py
+++ app/signatures/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from shortcuts import route
-from views import new, create
+from views import new, create, index
 
 
 urlpatterns = patterns('',
-    route(r'^signup/', GET=new, POST=create, name="signup")
+    route(r'^signup/', GET=new, POST=create, name="signup"),
+    url(r'^$', index, name="signatures-list")
 )
--- app/signatures/views.py
+++ app/signatures/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
+from models import Signatory
 from forms import SignatoryForm
 
 def new(request):
@@ -16,3 +18,10 @@ def create(request):
         return render_to_response('signatures/form.html', {'form': form})
     return render_to_response('signatures/signed.html')
 
+def index(request):
+    signatures = Signatory.objects.filter(is_active=True)
+    context = {
+        'MEDIA_URL': settings.MEDIA_URL,
+        'signatures': signatures
+    }
+    return render_to_response('signatures/list.html', context)
