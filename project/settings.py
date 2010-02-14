56cbee6fc43654e0bf5b45685d40bd9e0ea3bb5b
Henrique Bastos <henrique@bastos.net>
2010-02-14 18:59:27 -0200

    Fixed django language to en-us instead of pt-br.

--- project/settings.py
+++ project/settings.py
@@ -19,7 +19,7 @@ TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
