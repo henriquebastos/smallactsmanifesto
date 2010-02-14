fda95d31e9b1581be0040309d31172b58706a47b
Henrique Bastos <henrique@bastos.net>
2010-02-14 19:06:50 -0200

    Added test for valid form post to signup.

--- app/signatures/tests.py
+++ app/signatures/tests.py
@@ -65,3 +65,14 @@ class TestViews(TestCase):
         response = self.client.get(reverse('signup'))
         self.assertTemplateUsed(response, 'signatures/form.html')
 
+    def test_valid_form_signup(self):
+        post_data = {
+            'name': 'Henrique Bastos',
+            'email': 'henrique@bastos.net',
+            'url': 'http://henriquebastos.net',
+            'location': 'Rio de Janeiro/Brazil',
+        }
+        response = self.client.post(reverse('signup'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'signatures/signed.html')
+
