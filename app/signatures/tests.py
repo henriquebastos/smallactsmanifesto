96b2b8ff89aea4c8d1e03c1262a76cfc2df56e74
Henrique Bastos <henrique@bastos.net>
2010-02-14 14:01:19 -0200

    Added Signatory model with tests

--- app/signatures/tests.py
+++ app/signatures/tests.py
@@ -1,23 +1,47 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
+from datetime import datetime as dt
 
 from django.test import TestCase
+from django.db import IntegrityError
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from models import Signatory
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
+def factory(**kwargs):
+    param = dict(name="Henrique Bastos", 
+                 email="henrique@bastos.net",
+                 url="http://henriquebastos.net", 
+                 location="Rio de Janeiro/Brazil")
+    param.update(**kwargs)
+    return Signatory(**param)
 
+class TestModels(TestCase):
+    def test_add_new_signatory(self):
+        s = factory()
+        s.save()
+        self.assertEquals(s.id, 1L)
+        self.assertTrue(isinstance(s.signed_at, dt))
+
+    def test_raises_on_duplicated_email(self):
+        s1 = factory()
+        s2 = factory()
+
+        s1.save()
+        self.assertEquals(s1.id, 1L)
+        self.assertRaises(IntegrityError, s2.save)
+
+    def test_add_with_empty_url(self):
+        s = factory(url="")
+        s.save()
+        self.assertEquals(s.id, 1L)
+
+    def test_raises_on_blank_name(self):
+        s = factory(name=None)
+        self.assertRaises(IntegrityError, s.save)
+
+    def test_raises_on_blank_email(self):
+        s = factory(email=None)
+        self.assertRaises(IntegrityError, s.save)
+
+    def test_raises_on_blank_location(self):
+        s = factory(location=None)
+        self.assertRaises(IntegrityError, s.save)
