d7fc749beb2bf16f7dc20fe3abe4c418e2bd0945
Henrique Bastos <henrique@bastos.net>
2010-02-14 21:46:16 -0200

    Added url route to confirm_email

--- app/signatures/urls.py
+++ app/signatures/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from shortcuts import route
-from views import new, create, index
+from views import new, create, index, confirm_email
 
 
 urlpatterns = patterns('',
-    route(r'^signup/', GET=new, POST=create, name="signup"),
-    url(r'^$', index, name="signatures-list")
+    route(r'^signup/$', GET=new, POST=create, name='signup'),
+    url(r'^$', index, name='signatures-list'),
+    url(r'^signup/confirm_email/(\w+)/$', confirm_email, name='confirm_email'),
 )
