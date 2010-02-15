ae3e17da8a297359f4bd8e564bc15ba71f1a9691
Henrique Bastos <henrique@bastos.net>
2010-02-14 22:30:49 -0200

    Added url to debug signed.html

--- app/signatures/urls.py
+++ app/signatures/urls.py
@@ -9,3 +9,9 @@ urlpatterns = patterns('',
     url(r'^$', index, name='signatures-list'),
     url(r'^signup/confirm_email/(\w+)/$', confirm_email, name='confirm_email'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.generic.simple',
+        (r'^signed/$', 'direct_to_template', {'template': 'signatures/signed.html'},
+            {'MEDIA_URL': settings.MEDIA_URL}),
+    )
