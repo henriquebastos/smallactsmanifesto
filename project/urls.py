fb416e224857a79816bd4cca1c4b456e177d164a
Henrique Bastos <henrique@bastos.net>
2010-04-19 01:54:56 -0300

    Badges page simplified

--- project/urls.py
+++ project/urls.py
@@ -13,8 +13,25 @@ urlpatterns = patterns('',
     (r'^', include('signatures.urls')),
 )
 
-urlpatterns+= patterns('django.views.generic.simple',
-    (r'^community-badges/$', 'direct_to_template', {'template': 'community-badges.html'}),
+# BADGES
+badges = (
+    { 'size': '80x15', 'name': 'light default', 'sufix': '80x15' },
+    { 'size': '80x15', 'name': 'dark blue', 'sufix': '80x15-blue' },
+    { 'size': '88x31', 'name': 'light default', 'sufix': '88x31' },
+    { 'size': '88x31', 'name': 'dark blue', 'sufix': '88x31-blue' },
+    { 'size': '110x32', 'name': 'light default', 'sufix': '110x32' },
+    { 'size': '110x32', 'name': 'dark blue', 'sufix': '110x32-blue' },
+    { 'size': '120x60', 'name': 'light default', 'sufix': '120x60' },
+    { 'size': '120x60', 'name': 'dark blue', 'sufix': '120x60-blue' },
+)
+
+urlpatterns += patterns('django.views.generic.simple',
+    (r'^community-badges/$', 'direct_to_template', 
+        {
+            'template': 'community-badges.html', 
+            'extra_context': { 'badges': badges },
+        }
+    ),
 )
 
 if settings.DEBUG:
