b43642fd5c767ef9686403f7de5a9caa74c86353
Henrique Bastos <henrique@bastos.net>
2010-11-16 08:18:44 -0200

    Fix pythonpath issues.

--- project/manage.py
+++ project/manage.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+import os, sys
 from django.core.management import execute_manager
+
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), os.path.pardir)
+
+sys.path.insert(0, PROJECT_ROOT)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'app'))
+
 try:
-    import settings # Assumed to be in the same directory.
+    from project import settings # Assumed to be in the same directory.
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
