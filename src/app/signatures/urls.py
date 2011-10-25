424bcbf2a9d14082c6c2c5baa466336b9c194ec6
Henrique Bastos <henrique@bastos.net>
2011-10-25 17:38:13 -0200

    Use views as strings avoiding imports.

--- src/app/signatures/urls.py
+++ src/app/signatures/urls.py
@@ -1,13 +1,12 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from views import new, create, index, confirm_email
 from app.shortcuts import route
 
 
-urlpatterns = patterns('',
-    route(r'^signup/$', GET=new, POST=create, name='signup'),
-    url(r'^$', index, name='signatures-list'),
-    url(r'^signup/confirm_email/(\w+)/$', confirm_email, name='confirm_email'),
+urlpatterns = patterns('app.signatures.views',
+    route(r'^signup/$', GET='new', POST='create', name='signup'),
+    url(r'^$', 'index', name='signatures-list'),
+    url(r'^signup/confirm_email/(\w+)/$', 'confirm_email', name='confirm_email'),
 )
 
 #if settings.DEBUG:
