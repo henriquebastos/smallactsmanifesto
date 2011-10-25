d643642b1a9ee8e434c3fd3ad521c40f8d7ab83f
Henrique Bastos <henrique@bastos.net>
2011-10-25 18:20:04 -0200

    Uses views.generic.simple

--- src/app/signatures/views.py
+++ src/app/signatures/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render_to_response, redirect
+from django.views.generic.simple import direct_to_template, redirect_to
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
@@ -11,31 +11,34 @@ from utils import send_email_confirmation
 def new(request):
     form = SignatoryForm()
     context = {
-        'MEDIA_URL': settings.MEDIA_URL,
         'form': form
     }
-    return render_to_response('signatures/form.html', context)
+    return direct_to_template(request, 'signatures/form.html', context)
+
 
 def create(request):
     form = SignatoryForm(request.POST)
+
     context = {
-        'MEDIA_URL': settings.MEDIA_URL,
         'form': form
     }
+
     try:
         signatory = form.save_if_new()
         send_email_confirmation(signatory)
     except ValueError:
-        return render_to_response('signatures/form.html', context)
-    return render_to_response('signatures/signed.html', context)
+        return direct_to_template(request, 'signatures/form.html', context)
+
+    return direct_to_template(request, 'signatures/signed.html', context)
+
 
 def index(request):
     signatures = Signatory.objects.filter(is_active=True)
     context = {
-        'MEDIA_URL': settings.MEDIA_URL,
         'signatures': signatures
     }
-    return render_to_response('signatures/list.html', context)
+    return direct_to_template(request, 'signatures/list.html', context)
+
 
 def confirm_email(request, confirmation_key):
     try:
@@ -46,8 +49,7 @@ def confirm_email(request, confirmation_key):
         raise Http404
 
     context = {
-        'MEDIA_URL': settings.MEDIA_URL,
         'signatory': signatory,
     }
-    return render_to_response('signatures/confirmed.html', context)
+    return direct_to_template(request, 'signatures/confirmed.html', context)
 
