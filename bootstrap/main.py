7eb69e647a3dba3df4638e6d0c2929b257c8322c
Henrique Bastos <henrique@bastos.net>
2012-09-07 16:45:03 -0400

    Remove old bootstrap.

--- bootstrap/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-import virtualenv
-
-extension = open('venv-extension.py').read()
-output = virtualenv.create_bootstrap_script(extension)
-
-version = sys.version_info
-f = open('bootstrap%d.%d.py' % (version.major, version.minor), 'w').write(output)
