ac7a756b00bb06b4dc1bea25b556d03774e4099c
Henrique Bastos <henrique@bastos.net>
2012-09-09 18:24:15 -0400

    Refactor send confirmation email.

--- smallacts/signatures/views.py
+++ smallacts/signatures/views.py
@@ -1,23 +1,40 @@
 # coding: utf-8
+from django.conf import settings
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse as r
+from django.template.loader import render_to_string
 from django.views.generic import CreateView
 from django.views.generic import TemplateView
 from .forms import SignupForm
-from .utils import send_email_confirmation
 
 
 class SignupView(CreateView):
     "Shows the signup form and saves it's submission."
     form_class = SignupForm
     template_name = 'signatures/signup_form.html'
+    template_email_subject = 'signatures/signup_email_subject.txt'
+    template_email_body = 'signatures/signup_email_body.txt'
 
     def get_success_url(self):
         return r('signatures:success')
 
     def form_valid(self, form):
         response = super(SignupView, self).form_valid(form)
-        send_email_confirmation(self.object)
+        self.send_confirmation_email()
         return response
+
+    def send_confirmation_email(self):
+        """
+        Sends the signup confirmation email to the Signatory.
+        He will only be listed after access the confirmation link.
+        """
+        context = { object: self.object }
+        subject = render_to_string(self.template_email_subject, context)
+        message = render_to_string(self.template_email_body, context)
+
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL,
+                  [self.object.email, settings.DEFAULT_FROM_EMAIL])
+
 
 signup = SignupView.as_view()
 
