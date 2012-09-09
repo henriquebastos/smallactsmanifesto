f00ee5e3748f2af677971d3017ba44639ee6d43a
Henrique Bastos <henrique@bastos.net>
2012-09-09 18:38:05 -0400

    Add test for signup success page.

--- smallacts/signatures/tests/test_views.py
+++ smallacts/signatures/tests/test_views.py
@@ -66,3 +66,15 @@ class SignupViewPostSuccessTest(TestCase):
         'POST must send a copy of the feedback email to the admin.'
         self.assertIn(settings.DEFAULT_FROM_EMAIL, mail.outbox[0].to)
 
+
+class SignupSuccessViewTest(TestCase):
+    'Signup success GET tests.'
+    def setUp(self):
+        self.resp = self.client.get(r('signatures:success'))
+
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'signatures/signup_success.html')
+
