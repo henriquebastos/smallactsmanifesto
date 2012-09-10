ed159b5f5137dc91902419c52989f2f567801f11
Henrique Bastos <henrique@bastos.net>
2012-09-09 23:15:17 -0400

    Install south.

--- smallacts/settings.py
+++ smallacts/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'south',
     'smallacts.core',
     'smallacts.signatures',
 )
