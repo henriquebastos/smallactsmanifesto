0cd299410ae22a09259d3bf06e738e13eacaf79c
Henrique Bastos <henrique@bastos.net>
2012-09-10 00:55:04 -0400

    Activate admin urls.

--- smallacts/urls.py
+++ smallacts/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^signup/', include('smallacts.signatures.urls', namespace='signatures')),
     url(r'^', include('smallacts.core.urls', namespace='core')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
