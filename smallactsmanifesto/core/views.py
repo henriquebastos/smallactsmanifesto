0400d00ef1b476f31194345f8c1f6f10c1bb8467
Henrique Bastos <henrique@bastos.net>
2013-03-28 00:08:01 -0300

    Fix more references to old package

--- smallactsmanifesto/core/views.py
+++ smallactsmanifesto/core/views.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from django.views.generic.simple import direct_to_template
-from smallacts.signatures.models import Signatory
+from smallactsmanifesto.signatures.models import Signatory
 
 
 def index(request):
--- smallactsmanifesto/signatures/urls.py
+++ smallactsmanifesto/signatures/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from .views import SignupView, ConfirmView, SignupSuccessView
 
 
-urlpatterns = patterns('smallacts.signatures.views',
+urlpatterns = patterns('smallactsmanifesto.signatures.views',
     url(r'^$', SignupView.as_view(), name='signup'),
     url(r'^success/$', SignupSuccessView.as_view(), name='success'),
     url(r'^confirm/(?P<slug>\w+)/$', ConfirmView.as_view(), name='confirm'),
