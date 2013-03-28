79c693396d7b764231fd886ab0b362762796f3cb
Henrique Bastos <henrique@bastos.net>
2013-03-27 23:58:12 -0300

    Add code to activate virtualenv inside passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,8 +2,14 @@
 import os
 import sys
 
-
 PROJECT_ROOT = os.path.dirname(__file__)
+
+this_file = os.path.join(PROJECT_ROOT, 'bin', 'activate_this.py')
+execfile(this_file, dict(__file__=this_file))
+
+#INTERP = os.path.join(PROJECT_ROOT, 'bin', 'python')
+#INTERP is present twice so that the new python interpreter knows the actual executable path
+#if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 sys.path.insert(0, PROJECT_ROOT)
 
