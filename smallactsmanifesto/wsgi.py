9d8c523f76be38b6b062a478078a14f1cb620a40
Henrique Bastos <henrique@bastos.net>
2013-03-27 23:20:31 -0300

    Fix wgsi's seetings reference

--- smallactsmanifesto/wsgi.py
+++ smallactsmanifesto/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for smallacts project.
+WSGI config for smallactsmanifesto project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smallacts.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smallactsmanifesto.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
