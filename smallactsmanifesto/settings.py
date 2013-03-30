24ef2b2c4a771599adb5c21602b1b38c4aa3d83e
Henrique Bastos <henrique@bastos.net>
2013-03-30 18:39:11 -0300

    Configure cache

--- smallactsmanifesto/settings.py
+++ smallactsmanifesto/settings.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from itertools import chain
 from dj_database_url import parse as db_url
 from unipath import Path
 from smallactsmanifesto.config import DjangoConfig
@@ -55,3 +56,35 @@ EMAIL_PORT = config('EMAIL_PORT', default=25, type=int)
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, type=bool)
+
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+
+if config('CACHE_ENABLED', False, bool):
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+            'LOCATION': PROJECT_ROOT.child('cache'),
+            'TIMEOUT': 60 * 5,
+            }
+    }
+
+    MIDDLEWARE_CLASSES = tuple(chain(
+        (
+            'django.middleware.cache.UpdateCacheMiddleware',
+        ),
+        MIDDLEWARE_CLASSES,
+        (
+            'django.middleware.http.ConditionalGetMiddleware',
+            'django.middleware.gzip.GZipMiddleware',
+            'django.middleware.cache.FetchFromCacheMiddleware',
+        ),
+    ))
+
