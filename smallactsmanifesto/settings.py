6d6e061737fb4f6b15c2db894b78cd7530eb9f69
Henrique Bastos <henrique@bastos.net>
2014-02-25 03:05:53 -0300

    Replace django-decouple with python-decouple

--- smallactsmanifesto/settings.py
+++ smallactsmanifesto/settings.py
@@ -2,11 +2,9 @@
 from itertools import chain
 from dj_database_url import parse as db_url
 from unipath import Path
-from decouple import Config
+from decouple import config
 
 PROJECT_ROOT = Path(__file__).parent
-
-config = Config(PROJECT_ROOT.child('settings.ini'))
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 TEMPLATE_DEBUG = DEBUG
@@ -65,8 +63,8 @@ DEFAULT_FROM_EMAIL = 'admin@smallactsmanifesto.org'
 
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 
 
